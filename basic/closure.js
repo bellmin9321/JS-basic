da8cb4930bcb98bb4ccc9494781bf565d58170ae
JONGMIN KIM <bellmin9321@gmail.com>
2022-03-18 17:29:18 +0900

    add curry example

--- basic/closure.js
+++ basic/closure.js
@@ -28,5 +28,37 @@ function bar(fn) {
 }
 
 getFruit();
+// -----------------------
 
+function addCurry(x) {
+  return function add(y) {
+    return x + y;
+  };
+}
 
+const addFive = addCurry(5);
+const addTen = addCurry(10);
+
+const result1 = addTen(20);
+const result2 = addFive(5);
+const result3 = addTen(10);
+
+console.log(result1, result2, result3);
+
+function _curry(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return fn(a, b, c)
+      };
+    };
+  };
+}
+
+const addArgs = function add(a, b, c) {
+  return a + b + c;
+}
+
+const result = _curry(addArgs);
+
+console.log(result(1)(2)(3));
